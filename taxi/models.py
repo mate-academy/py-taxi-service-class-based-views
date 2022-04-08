2cf7a9d514bbaf93086c444643abcbd5d4d28517
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-04-08 17:06:14 +0300

    'Solution'

--- taxi/models.py
+++ taxi/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
 
 
 class Driver(AbstractUser):
@@ -14,8 +21,14 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return self.model
