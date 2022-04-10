91a8ccfb83ed51cf1e9622189f2948cddfff5077
unknown <arthuromgnh@gmail.com>
2022-04-10 21:14:20 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path('manufacturers/', ManufacturerListView.as_view()),
+    path('cars/', CarListView.as_view()),
+    path('cars/<int:pk>/', CarDetailView.as_view()),
+    path('drivers/', DriverListView.as_view()),
+    path('drivers/<int:pk>/', DriverDetailView.as_view())
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Driver, Car, Manufacturer
 
@@ -17,3 +18,38 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+
+    context_object_name = 'manufacturers'
+    queryset = Manufacturer.objects.all()
+    paginate_by = 2
+    template_name = 'taxi/manufacturer_list.html'
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related('manufacturer')
+    context_object_name = 'cars'
+    paginate_by = 3
+    template_name = 'taxi/car_list.html'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'taxi/car_detail.html'
+    queryset = Car.objects.prefetch_related('drivers').select_related('manufacturer')
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 4
+    template_name = 'taxi/driver_list.html'
+    context_object_name = 'drivers'
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = 'taxi/driver_detail.html'
+    context_object_name = 'driver'
