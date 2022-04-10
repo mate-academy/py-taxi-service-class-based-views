d9fb715a65196a4d049a1cc799456720f402b506
Nazar-kob <kobrinnazar05@mail.com>
2022-04-10 12:29:18 +0300

    nazarKobryn

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView,\
+    CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/",  ManufacturerListView.as_view(), name='manufacturers-list'),
+    path("cars/",  CarListView.as_view(), name='cars-lit'),
+    path("cars/<int:pk>", CarDetailView.as_view(), name='car_detail'),
+    path("drivers/", DriverListView.as_view(), name='drivers-list'),
+    path("drivers/<int:pk>", DriverDetailView.as_view(), name='driver_detail')
+
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Driver, Car, Manufacturer
 
@@ -17,3 +18,32 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = 'taxi/manufacturers_list.html'
+    context_object_name = 'manufacturers'
+    queryset = Manufacturer.objects.order_by('name')
+    paginate_by = 2
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related('manufacturer')
+    paginate_by = 3
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 2
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all().prefetch_related('cars__manufacturer')
+
