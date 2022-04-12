3a80a7406887077293bf25e7c40dfe1e5650d37a
Nazar-kob <kobrinnazar05@mail.com>
2022-04-12 17:22:45 +0300

    changed name

--- taxi/urls.py
+++ taxi/urls.py
@@ -5,10 +5,10 @@ from .views import index, ManufacturerListView, CarListView,\
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/",  ManufacturerListView.as_view(), name='manufacturers-list'),
-    path("cars/",  CarListView.as_view(), name='cars-lit'),
+    path("manufacturers/",  ManufacturerListView.as_view(), name='manufacturers_list'),
+    path("cars/",  CarListView.as_view(), name='cars_lit'),
     path("cars/<int:pk>", CarDetailView.as_view(), name='car_detail'),
-    path("drivers/", DriverListView.as_view(), name='drivers-list'),
+    path("drivers/", DriverListView.as_view(), name='drivers_list'),
     path("drivers/<int:pk>", DriverDetailView.as_view(), name='driver_detail')
 
 ]
