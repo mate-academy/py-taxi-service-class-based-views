7374a9b3c414a56e4c75688e260af43b61797dee
TJgkOsVP <sasha06052001@gmail.com>
2022-04-12 11:37:50 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path(
         "manufacturers/", ManufacturerListView.as_view(), name="taxi-manufacturer-list"
     ),
-    path("cars/", CarListView.as_view(), name="taxi-car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="taxi-car-detail"),
-    path("drivers/", DriverListView.as_view(), name="taxi-driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="taxi-driver-detail"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
 app_name = "taxi"
