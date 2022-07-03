e15f7e08dbd094f4c2dd3645bc7b4e12477c58c8
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-07-03 17:21:36 +0300

    'AddMetaForCar'

--- taxi/models.py
+++ taxi/models.py
@@ -25,3 +25,6 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ["model"]
