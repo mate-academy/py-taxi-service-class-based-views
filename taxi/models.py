1c25f79827989b4e2d0d446ba71a985c52682fc7
Artem Toporchan <gydratvar@gmail.com>
2022-07-05 00:31:02 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -6,6 +6,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
@@ -13,6 +16,7 @@ class Driver(AbstractUser):
     class Meta:
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+        ordering = ["username"]
 
 
 class Car(models.Model):
