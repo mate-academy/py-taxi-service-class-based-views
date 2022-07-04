f720844a383f62698c19d2645242e2ee2f03b784
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-07-04 21:00:11 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView, CarDetailView, DriverDetailView, DriverListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(template_name='taxi/manufacturer_list.html'),
+         name='manufacturers'),
+    path("cars/", CarListView.as_view(template_name='taxi/car_list.html'),
+         name='cars'),
+    path("drivers/", DriverListView.as_view(template_name='taxi/driver_list.html'),
+         name='drivers'),
+    path("cars/<int:pk>/", CarDetailView.as_view(template_name='taxi/car_detail.html'),
+         name='car_detail'),
+    path("driver/<int:pk>/", DriverDetailView.as_view(template_name='taxi/driver_detail.html'),
+         name='driver_detail'),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
@@ -17,3 +16,28 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    paginate_by = 2
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 2
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 2
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
