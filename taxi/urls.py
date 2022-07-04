cfb095cb2fe106cccb54be2ce0eacee542ead8b7
Roman <romanschneider9@gmail.com>
2022-07-04 15:22:40 +0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,45 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverDetailView,
+    DriverListView,
+)
 
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "",
+        index,
+        name="index"
+    ),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturers-list"
+    ),
+    path(
+        "cars/",
+         CarListView.as_view(),
+        name="cars-list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="drivers-list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
 ]
 
 app_name = "taxi"
