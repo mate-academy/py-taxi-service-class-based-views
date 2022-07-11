e27178e9fe60fa101f54e758f7cd40458cd36200
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-07-11 17:48:56 +0300

    Update urls.py
    quotes changed

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,15 +6,15 @@ from .views import index, ManufacturerListView, CarListView, CarDetailView, \
 urlpatterns = [
     path("", index, name="index"),
     path("manufacturers/", ManufacturerListView.as_view(),
-         name='manufacturers'),
+         name="manufacturers"),
     path("cars/", CarListView.as_view(),
-         name='cars'),
+         name="cars"),
     path("drivers/", DriverListView.as_view(),
-         name='drivers'),
+         name="drivers"),
     path("cars/<int:pk>/", CarDetailView.as_view(),
          name='car_detail'),
     path("driver/<int:pk>/", DriverDetailView.as_view(),
-         name='driver_detail'),
+         name="driver_detail"),
 ]
 
 app_name = "taxi"
