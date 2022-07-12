9e8e372a5f9713413b71f88fd1768b2ab059e216
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-07-12 21:35:00 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,21 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
+        ordering = ["username"]
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ["model"]
+
+    def get_absolute_url(self):
+        return reverse("taxi:cars-detail", args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.model} ({self.manufacturer.name})"
--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,35 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView, CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "Manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturers-list"
+    ),
+    path(
+        "Cars/",
+        CarListView.as_view(),
+        name="cars-list"
+    ),
+    path(
+        "Cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="cars-detail"
+    ),
+    path(
+        "Drivers/",
+        DriverListView.as_view(),
+        name="drivers-list"
+    ),
+    path(
+        "Drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="drivers-detail"
+    ),
 ]
+
 
 app_name = "taxi"
