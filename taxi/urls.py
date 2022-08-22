3858bb031942c1cf1ac0ba367fd3490f5ec4703e
dstarkaiev <dstarkaiev@gmail.com>
2022-08-22 15:37:29 +0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,10 +4,10 @@ from .views import index, ManufacturerListView, CarListView, DriverListView, Car
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
-    path("cars/", CarListView.as_view(), name="car-list"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer_list"),
+    path("cars/", CarListView.as_view(), name="car_list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/", DriverListView.as_view(), name="driver_list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver_detail.html"),
 ]
 
