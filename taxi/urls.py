012ccfb58dc6ba80fba5517a850438b25e38422a
Denis <sairustv228@gmail.com>
2022-09-28 10:01:41 +0300

    Sulution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,39 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from .views import index
+from .views import index, \
+    ManufacturerListView, \
+    CarListView, \
+    CarDetailView, DriverDetailView, DriverListView
 
 urlpatterns = [
     path("", index, name="index"),
-]
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer_list"
+    ),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car_list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car_detail"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver_list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver_detail"
+    )
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "taxi"
