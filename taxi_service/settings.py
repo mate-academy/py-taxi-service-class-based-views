0fe5edcd3e9a13056599214ed7e7a49cd5519580
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-10-10 13:07:56 +0300

    'Solution'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
     # ...
-    "127.0.0.1",
+    # "127.0.0.1",
     # ...
 ]
 # Application definition
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
+    # "debug_toolbar",
     "taxi",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
