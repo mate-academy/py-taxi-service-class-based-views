db0b9fbe0da1758844b71a08ba491d194ca3f1af
Vladyslav Peresta <perestavlad@gmail.com>
2022-10-20 13:39:55 +0300

    Fixed line length

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from .views import (index, ManufacturerListView,
-                    CarListView, CarDetailView, DriverListView, DriverDetailView)
+                    CarListView, CarDetailView,
+                    DriverListView, DriverDetailView)
 
 urlpatterns = [
     path("", index, name="index"),
