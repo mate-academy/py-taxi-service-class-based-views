62f8db4801940c9d004e61d36323ad31cd2d1bda
Inna Kushnir <innakushnir63@gmail.com>
2022-10-24 15:28:42 +0300

    Solutions

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -146,11 +146,3 @@ def show_toolbar(request):
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
 }
-
-
-import mimetypes
-mimetypes.add_type("application/javascript", ".js", True)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
