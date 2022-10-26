835c78ddf0a1414b0a0db42332a32c4a7a9f3664
Ivan Antonishak <43565760+IvanStored@users.noreply.github.com>
2022-10-26 18:00:27 +0300

    Update views.py

--- taxi/views.py
+++ taxi/views.py
@@ -22,7 +22,7 @@ def index(request):
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
@@ -37,7 +37,7 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__drivers")
+    queryset = Driver.objects.prefetch_related("cars__drivers")
 
 
 class ManufacturerListView(generic.ListView):
