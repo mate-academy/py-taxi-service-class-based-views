e7b97af6d4110ff1c8cdcd8bd43eb2a13744bf03
Alexander <alexzubkoff123@gmail.com>
2022-11-03 01:13:58 +0200

    Created class based views, pagination

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Driver, Car, Manufacturer
 
@@ -17,3 +18,41 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+def manufacturer_list_view(request):
+    manufacturer_list = Manufacturer.objects.all()
+
+    context = {
+        "manufacturer_list": manufacturer_list,
+    }
+    return render(request, "taxi/manufacturer_list.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    context_object_name = "car_list"
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    context_object_name = "driver_list"
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
