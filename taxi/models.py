c37ce73894d175c8bf6ac9b9618690f63160142e
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-02 20:45:54 +0200

    develop

--- taxi/models.py
+++ taxi/models.py
@@ -6,6 +6,12 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
@@ -14,8 +20,17 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ["model"]
+
+    def __str__(self):
+        return f"{self.manufacturer} {self.model}"
