e17096dba610dd72248cc9c76e9d8903e8808eac
Roman Kolmakov <cerne313@gmail.com>
2022-11-03 19:09:45 +0200

    implement templates functionality (except includes)

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,11 +11,11 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturers_list"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer_list"),
     path("cars/", CarListView.as_view(), name="cars_list"),
-    path("cars/<int: pk>/", CarDetailView.as_view(), name="car_detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
     path("drivers/", DriverListView.as_view(), name="drivers_list"),
-    path("drivers/<int: pk>/", DriverDetailView.as_view(), name="driver_detail")
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver_detail")
 ]
 
 app_name = "taxi"
