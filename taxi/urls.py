38be82d5d9b42120d87de0f165f41ac18789dbae
Eugene <buntyc8@gmail.com>
2022-11-16 10:38:56 +0200

    fix indexation

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, ManufacturerListView, \
+from .views import ManufacturerListView, \
     CarListView, DriverListView, DriverDetailView, CarDetailView
 
 urlpatterns = [
