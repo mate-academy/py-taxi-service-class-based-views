e9a53aa889b581864c0761527b4ca72f2d9fca41
George <georgepavlej@gmail.com>
2022-12-01 21:17:32 +0000

    Created class based views and templates for those views

--- taxi/models.py
+++ taxi/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Driver(AbstractUser):
@@ -14,8 +18,20 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", args=[str(self.id)])
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ["model"]
+
+    def get_absolute_url(self):
+        return reverse("taxi:car-detail", args=[str(self.id)])
+
+    def __str__(self) -> str:
+        return f"{self.manufacturer} {self.model}"
