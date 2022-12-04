30f15b062c71fcf8e23584da5f3701d9b70115b1
IvanGLS <gladiolus0707@gmail.com>
2022-12-04 16:20:04 +0200

    'Solution'

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import index, \
-    ManufacturerListView, \
-    CarListView, \
-    CarDetailView, \
-    DriverDetailView, \
-    DriverListView
+from .views import (index,
+                    ManufacturerListView,
+                    CarListView,
+                    CarDetailView,
+                    DriverDetailView,
+                    DriverListView,)
 
 urlpatterns = [
     path("", index, name="index"),
