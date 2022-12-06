94e0697b1116f482760e030172722f7442c66913
Diana <daria.blessed18@gmail.com>
2022-12-06 12:40:35 +0200

    template_name deleted

--- taxi/views.py
+++ taxi/views.py
@@ -23,13 +23,11 @@ def index(request):
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
     queryset = Manufacturer.objects.order_by("name")
-    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    template_name = "taxi/car_list.html"
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
 
@@ -43,7 +41,6 @@ class CarDetailView(generic.DetailView):
 
 class DriverListView(generic.ListView):
     model = Driver
-    template_name = "taxi/driver_list.html"
     queryset = Driver.objects.prefetch_related("cars")
     paginate_by = 5
 
