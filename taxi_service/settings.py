b487295fd2bca62ce45e06e7dd4334d8a7bf4c79
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-12-20 15:26:14 +0200

    debug_toolbar was removed

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -42,12 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taxi",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
