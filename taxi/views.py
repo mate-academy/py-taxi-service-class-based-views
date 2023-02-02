e3aa18a42d699ab8c62316cdcdc998faa21944fb
Ihor Ivashov <dm-i@i.ua>
2023-02-02 16:32:11 +0200

    Changed views

--- taxi/views.py
+++ taxi/views.py
@@ -22,14 +22,14 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
