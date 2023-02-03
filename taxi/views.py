78e176b2aa33f63149823333a189757fe69c198a
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-02-03 10:25:17 +0800

    Used parenthesis to split line in imports, set context_object_name and template_name for manufacturers and set queryset with needed method in it for DriverDetailView

--- taxi/views.py
+++ taxi/views.py
@@ -22,6 +22,8 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
     paginate_by = 5
 
 
@@ -42,3 +44,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
+    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
