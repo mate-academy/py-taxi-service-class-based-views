8617375f2bae623674369ebd279ecb62389cd9b6
muhactive <muhactive@gmail.com>
2023-02-20 12:52:26 +0200

    Delete all() in queriset

--- taxi/views.py
+++ taxi/views.py
@@ -27,7 +27,7 @@ class DriverListView(generic.ListView):
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
@@ -38,7 +38,7 @@ class ManufacturerListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars")
 
 
 class CarDetailView(generic.DetailView):
