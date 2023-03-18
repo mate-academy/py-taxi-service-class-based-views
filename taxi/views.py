727c1536db9fa815ac259f1a35142a04ef4ca240
Paul Maslov <maslovp67@gmail.com>
2023-03-18 18:10:41 +0200

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -38,6 +38,7 @@ class CarListView(generic.ListView):
 
 class CarDetailView(generic.DetailView):
     model = Car
+    queryset = Car.objects.prefetch_related("drivers")
 
 
 class DriverListView(generic.ListView):
@@ -48,4 +49,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars")
