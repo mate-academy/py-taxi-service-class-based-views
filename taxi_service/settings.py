22b5fe6b3f709680c591e8a6ce754657d0f262ee
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-03-21 21:33:18 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "taxi",
     "debug_toolbar",
+    "taxi",
+
 ]
 
 MIDDLEWARE = [
