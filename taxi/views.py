d56c6386724c25a20b1eff8e9c956a398a059dc7
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-03-23 20:23:11 +0200

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -22,8 +22,6 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_list.html"
-    context_object_name = "manufacturer_list"
     paginate_by = 5
     queryset = Manufacturer.objects.order_by("name")
 
