80031571ab6da4b5420d53a0ab0f22a814d8d1bc
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-27 18:23:56 +0300

    Fixed paths

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,10 +15,26 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list"
     ),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
 
 ]
 
