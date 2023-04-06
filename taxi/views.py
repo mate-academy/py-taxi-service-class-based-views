b16b96d4ca16f5fad5deee7b1affdeb28689bf04
Oleksandr <lotrikys@gmail.com>
2023-04-06 19:16:24 +0300

    Fix

--- taxi/views.py
+++ taxi/views.py
@@ -30,7 +30,7 @@ class ManufacturerListView(generic.ListView):
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
@@ -45,4 +45,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related()
+    queryset = Driver.objects.prefetch_related()
