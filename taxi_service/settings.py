ef978d11d1da18459ac9611c5f95115986fa7f26
Taras Savchyn <savik1992@gmail.com>
2023-04-27 23:49:21 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -99,13 +99,17 @@ AUTH_PASSWORD_VALIDATORS = [
         "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
+    },
+
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation." "NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
