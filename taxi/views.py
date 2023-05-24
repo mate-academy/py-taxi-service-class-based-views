0314a36b08dc3e5375128e55fce0e8806d77546d
Kristina <tina.vasylenko@gmail.com>
2023-05-24 10:57:38 +0300

    Solution1

--- taxi/views.py
+++ taxi/views.py
@@ -6,11 +6,9 @@ from .models import Driver, Car, Manufacturer
 
 def index(request):
     """View function for the home page of the site."""
-
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
@@ -22,18 +20,15 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_list.html"
     context_object_name = "manufacturer_list"
-    queryset = Manufacturer.objects.order_by("name")
+    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    template_name = "taxi/car_list.html"
-    context_object_name = "car_list"
-    queryset = Car.objects.all().select_related("manufacturer")
     paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarDetailView(generic.DetailView):
@@ -42,10 +37,9 @@ class CarDetailView(generic.DetailView):
 
 class DriverListView(generic.ListView):
     model = Driver
-    template_name = "taxi/driver_list.html"
-    context_object_name = "driver_list"
     paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
     model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
