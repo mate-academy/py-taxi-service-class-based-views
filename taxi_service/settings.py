87b0fd6a15d9c5982546b2e5484776877b9a9230
Mazur Mariya <mazurinka29@gmail.com>
2023-06-12 13:23:58 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -28,9 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    "127.0.0.1.",
 ]
-
 
 # Application definition
 
@@ -132,6 +131,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
