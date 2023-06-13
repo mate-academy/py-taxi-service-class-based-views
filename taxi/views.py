4b18cdd47e869fb634c81b79e1f7546d9946a8b4
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-13 17:58:56 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,12 @@
+from typing import Callable
+
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Driver, Car, Manufacturer
 
 
-def index(request):
+def index(request) -> Callable:
     """View function for the home page of the site."""
 
     num_drivers = Driver.objects.count()
@@ -17,3 +20,27 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.all().select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
