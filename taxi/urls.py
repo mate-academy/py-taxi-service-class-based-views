e1532974538737eef48ea3866f70acd4871f2599
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-06-28 12:07:09 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,36 @@
 from django.urls import path
 
-from .views import index
+from taxi.views import index, manufacturer_list_view, \
+    CarListView, CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturer/",
+        manufacturer_list_view,
+        name="manufacturer-list"
+    ),
+    path(
+        "car/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "car/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "driver/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "driver/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from django.core.paginator import Paginator
 
 from .models import Driver, Car, Manufacturer
 
@@ -17,3 +19,41 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+def manufacturer_list_view(request):
+    manufacturer_list = Manufacturer.objects.all().order_by("name")
+    paginator = Paginator(manufacturer_list, 5)
+    page = request.GET.get("page")
+    manufacturer_list_on_page = paginator.get_page(page)
+
+    context = {
+        "manufacturer_list": manufacturer_list_on_page,
+        "page": page,
+    }
+    return render(
+        request,
+        "taxi/manufacturer_list.html",
+        context=context
+    )
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.all().select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all().prefetch_related("cars")
+    paginate_by = 5
