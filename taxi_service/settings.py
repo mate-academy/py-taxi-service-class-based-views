32dc592e60b5515fcc32f95cc093d3e9e87cda4f
Volodymyr Bohomol <8773927@gmail.com>
2023-07-02 22:39:42 +0300

    Taxi service class-based views

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -27,10 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
 
 # Application definition
 
@@ -42,12 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taxi",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
