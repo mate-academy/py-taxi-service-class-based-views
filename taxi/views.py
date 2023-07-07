8a74aad831fc701edfb0dffad94810185b840423
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-07-07 13:29:43 +0300

    Add "select_related" and "prefetch_related" for optimize query

--- taxi/views.py
+++ taxi/views.py
@@ -34,6 +34,9 @@ class CarListView(generic.ListView):
 
 class CarDetailView(generic.DetailView):
     model = Car
+    queryset = (
+        Car.objects.select_related("manufacturer").prefetch_related("drivers")
+    )
 
 
 class DriverListView(generic.ListView):
