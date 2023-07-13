60edd476bc156eb722d701eb9fce0a68dbb945d0
Denys Seliverstov <catsperr322@gmail.com>
2023-07-13 16:42:11 +0300

    Fixed

--- taxi/urls.py
+++ taxi/urls.py
@@ -16,13 +16,13 @@ urlpatterns = [
     path("cars/",
          CarListView.as_view(),
          name="car-list"),
-    path("cars/<int:pk>",
+    path("cars/<int:pk>/",
          CarDetailView.as_view(),
          name="car-detail"),
     path("drivers/",
          DriverListView.as_view(),
          name="driver-list"),
-    path("drivers/<int:pk>",
+    path("drivers/<int:pk>/",
          DriverDetailView.as_view(),
          name="driver-detail")
 ]
