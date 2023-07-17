b0af938bc9aba2375973dadf71cae6c253d06555
YOUR_NAME <YOUR_EMAIL>
2023-07-17 20:56:33 +0300

    Add context_object_name, template_name in ManufacturerListView

--- taxi/views.py
+++ taxi/views.py
@@ -22,6 +22,8 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
+    context_object_name = "manufacturer_list"
+    template_name = "taxi/manufacturer_list.html"
     queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
