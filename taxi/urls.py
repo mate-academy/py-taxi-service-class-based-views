b4bb72d884bfa44793ccf436aaa9c1dc16c2669c
Munkoor <eee1lvlboss@gmail.com>
2023-08-02 21:48:06 +0300

    fixed flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -5,7 +5,8 @@ from .views import index, ManufacturerListView, CarListView, CarDetailView, \
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturer/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("manufacturer/", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
     path("car/", CarListView.as_view(), name="car-list"),
     path("car/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("driver/", DriverListView.as_view(), name="driver-list"),
