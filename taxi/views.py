a1213019a3072ece2b396236d61f0e54d21cd66f
stepband <vladshvik@gmail.com>
2023-08-07 17:22:30 +0300

    main functions implemented

--- taxi/views.py
+++ taxi/views.py
@@ -5,6 +5,7 @@ from .models import Driver, Car, Manufacturer
 
 
 def index(request):
+
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
@@ -20,33 +21,27 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    context_object_name = "manufacturers"
-    queryset = Manufacturer.objects.order_by("name")
+    context_object_name = "manufacturer_list"
     template_name = "taxi/manufacturer_list.html"
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    context_object_name = "cars"
-    template_name = "taxi/car_list.html"
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
 
 
 class CarDetailView(generic.DetailView):
     model = Car
-    template_name = "taxi/car_detail.html"
 
 
 class DriverListView(generic.ListView):
     model = Driver
-    template_name = "taxi/driver_list.html"
-    context_object_name = "drivers"
     paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    template_name = "taxi/driver_detail.html"
-    queryset = Driver.objects.all().select_related("cars__manufacturer")
+    queryset = Driver.objects.prefetch_related("cars")
