47db7791ca712c3628cd822f8a62a792ce9d936d
Vlad Kleshko <vkoesko@gmail.com>
2023-08-30 13:53:00 +0300

    i fixed tests

--- taxi/views.py
+++ taxi/views.py
@@ -24,6 +24,7 @@ class ManufacturerListView(generic.ListView):
     model = Manufacturer
     template_name = "taxi/manufacturer_list.html"
     context_object_name = "manufacturer_list"
+    paginate_by = 5
 
 
 class CarListView(generic.ListView):
