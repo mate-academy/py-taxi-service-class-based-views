3c979ca3c6cfc7cade69a5ced14a65ee4ce7602c
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2023-09-02 23:27:57 +0200

    Remove redundant code

--- taxi/views.py
+++ taxi/views.py
@@ -22,7 +22,7 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
