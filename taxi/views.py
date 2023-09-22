3739cae921124173bc6fe4d15035f233e36abe60
Yeva <yevabrookman@gmail.com>
2023-09-22 20:13:38 +0300

    Small fixes

--- taxi/views.py
+++ taxi/views.py
@@ -43,4 +43,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+    queryset = Driver.objects.prefetch_related("cars")
