59669bece64850eda79f72a9ad1834d4c19fab01
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-09-22 22:20:33 +0300

    improved appearence, refactored

--- taxi/views.py
+++ taxi/views.py
@@ -22,33 +22,26 @@ def index(request):
 
 class ManufacturerListView(ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
     queryset = Manufacturer.objects.order_by("name")
-    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
 class CarListView(ListView):
     model = Car
-    context_object_name = "car_list"
     queryset = Car.objects.order_by("model").select_related("manufacturer")
-    template_name = "taxi/car_list.html"
     paginate_by = 5
 
 
 class CarDetailView(DetailView):
     model = Car
-    template_name = "taxi/car_detail.html"
+    queryset = Car.objects.prefetch_related("drivers")
 
 
 class DriverListView(ListView):
     model = Driver
     paginate_by = 5
-    context_object_name = "driver_list"
-    template_name = "taxi/driver_list.html"
 
 
 class DriverDetailView(DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars")
-    template_name = "taxi/driver_detail.html"
