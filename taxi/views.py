87cf544c403683fa437b5c6dd6801dd2c2f4c59e
Max Katkalov <makskatkalov@gmail.com>
2023-09-22 19:22:41 +0300

    taxi-service-class-based-views implemented

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
 from .models import Driver, Car, Manufacturer
 
@@ -17,3 +19,60 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.all().order_by("name")
+    template_name = "taxi/manufacturer_list.html"
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = (
+        Car.objects.select_related("manufacturer").all().order_by("model")
+    )
+    template_name = "taxi/car_list.html"
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    context_object_name = "car"
+
+    def get_object(self, queryset=None):
+        return (
+            Car.objects.select_related("manufacturer")
+            .prefetch_related("drivers")
+            .get(id=self.kwargs["pk"])
+        )
+
+    def get_context_data(self, **kwargs):
+        car = self.object
+        context = super().get_context_data(
+            object_list=car.drivers.all(), **kwargs
+        )
+        return context
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView, MultipleObjectMixin):
+    model = Driver
+    context_object_name = "driver"
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        driver = self.get_object()
+        object_list = (
+            Driver.objects.prefetch_related("cars", "cars__manufacturer")
+            .get(id=driver.id)
+            .cars.all()
+        )
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        return context
