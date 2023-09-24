dca989bd0af9382fd998ab4e7e809afa9b7e23f0
Oleh Chernous <chernous.oleh@gmail.com>
2023-09-24 20:08:14 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -6,9 +6,21 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["last_name", ]
+
+    def __str__(self) -> str:
+        return f"{self.username} - {self.first_name} {self.last_name}"
 
 
 class Car(models.Model):
@@ -17,3 +29,9 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = ["model", ]
+
+    def __str__(self) -> str:
+        return f"{self.model}"
