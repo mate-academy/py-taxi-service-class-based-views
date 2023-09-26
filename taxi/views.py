39a2ece9c413d830833d00b753d812e0ca849a4f
Andrii Kurta <andriykurta97@gmail,com>
2023-09-26 08:23:59 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,43 +1,48 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from taxi.models import Driver, Car, Manufacturer
 
 
+@login_required
 def index(request):
     """View function for the home page of the site."""
-
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
+        "num_visits": num_visits + 1
     }
 
     return render(request, "taxi/index.html", context=context)
 
 
-class DriverListView(generic.ListView):
+class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
 
 
-class CarListView(generic.ListView):
+class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     queryset = Car.objects.order_by("model").prefetch_related("drivers")
     paginate_by = 5
 
 
-class ManufacturerListView(generic.ListView):
+class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
     queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
-class DriverDetailView(generic.DetailView):
+class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars")
 
 
-class CarDetailView(generic.DetailView):
+class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
     queryset = Car.objects.prefetch_related("drivers")
