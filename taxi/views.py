4897fb2e64d51465e49dafbee8e5a7c81ac428fa
Andrii Kurta <98520646+Kurta97@users.noreply.github.com>
2023-09-26 09:46:08 +0300

    Update views.py

--- taxi/views.py
+++ taxi/views.py
@@ -6,7 +6,7 @@ from taxi.models import Driver, Car, Manufacturer
 
 def index(request):
     """View function for the home page of the site."""
-    
+
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
