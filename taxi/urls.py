5d5329d46c66ed9eed6669603a88b1c9b0953384
Denys Raboshchuk <kontroler444@gmail.com>
2023-10-03 15:11:58 +0300

    Done!

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,27 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import index
+
+from .views import (
+    index, ManufacturerListView, CarListView,
+    CarDetailView, DriverListView, DriverDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 app_name = "taxi"
