cbe0f89c9118be3ea282e3168a2274b3005e68cc
Anastasia <anastasiiya.p@gmail.com>
2023-10-24 20:41:17 +0300

    Fixed

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -127,7 +126,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
