f0533a68a9eaadeeeb66c0afcc45191bbe047b25
Misha Shushman <endernagashushman@gmail.com>
2023-11-02 17:49:53 +0200

    added blank line to all html file

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView, CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturers"),
+    path("cars/", CarListView.as_view(), name="cars"),
+    path("cars/<int:pk>/", CarDetailView.as_view()),
+    path("drivers/", DriverListView.as_view(), name="drivers"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view())
 ]
 
 app_name = "taxi"
