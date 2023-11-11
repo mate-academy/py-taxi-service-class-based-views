c4c429f602114d17220d7241f9ae60b48d700756
Sergii Pryima <spryima@gmail.com>
2023-11-11 12:53:07 +0200

    queryset fixed

--- taxi/views.py
+++ taxi/views.py
@@ -31,6 +31,9 @@ class CarListView(generic.ListView):
 
 class CarDetailView(generic.DetailView):
     model = Car
+    queryset = (Car.objects
+                .select_related("manufacturer")
+                .prefetch_related("drivers"))
 
 
 class DriverListView(generic.ListView):
