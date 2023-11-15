2c906e5fa64b0556657d934d4bc02273b08ec553
Roman Muraviov <5079447@gmail.com>
2023-11-15 15:07:49 +0200

    + empty line, queryset changes

--- taxi/views.py
+++ taxi/views.py
@@ -31,9 +31,9 @@ class CarListView(generic.ListView):
 
 class CarDetailView(generic.DetailView):
     model = Car
-    queryset = (Car.objects
-                .select_related("manufacturer")
-                .prefetch_related("drivers"))
+    queryset = Car.objects.select_related(
+        "manufacturer"
+    ).prefetch_related("drivers")
 
 
 class DriverListView(generic.ListView):
