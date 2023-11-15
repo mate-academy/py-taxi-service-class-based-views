9d3b0c7e9a827ee8c0d79573e3ee742c86575421
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-11-15 20:31:50 +0200

    Added pagination

--- taxi/urls.py
+++ taxi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
-        name="manufacturers_list"
+        name="manufacturers-list"
     ),
     path("cars/", CarListView.as_view(), name="cars_list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
