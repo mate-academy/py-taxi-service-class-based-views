05991c7f340854bd873f460fcfa480e04aeeb877
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-11-15 14:38:35 +0200

    flake

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,11 +11,21 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name='manufacturer-list'),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("car/<int:pk>", CarDetailView.as_view(), name="car-detail"),
-    path("driver/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
+    path("manufacturers/",
+         ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path("cars/",
+         CarListView.as_view(),
+         name="car-list"),
+    path("drivers/",
+         DriverListView.as_view(),
+         name="driver-list"),
+    path("car/<int:pk>",
+         CarDetailView.as_view(),
+         name="car-detail"),
+    path("driver/<int:pk>",
+         DriverDetailView.as_view(),
+         name="driver-detail"),
 ]
 
 app_name = "taxi"
