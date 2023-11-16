fb16e61eca3d7a237b179411c1654aed6ce5400b
Rostyslav Chornyi <145897105+RVChornyy@users.noreply.github.com>
2023-11-16 14:21:52 +0200

    Update urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("cars/<int:pk>", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/<int:pk>", DriverDetailView.as_view(), name="driver-detail")
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail")
 ]
 
 app_name = "taxi"
