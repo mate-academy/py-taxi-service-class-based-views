157c113c6762f4683c42b6513db7b3b6bf39937d
nsdNite <nsd7night@gmail.com>
2023-11-19 11:37:29 +0200

    Solution taxi-service-class-based-view

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,37 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars", "cars__manufacturer")
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
 
     context = {
