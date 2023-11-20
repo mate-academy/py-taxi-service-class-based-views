9aee5ed237d2c1095254fa3557cb0368dc264f01
Artem Putsenko <artempucenko3@gmail.com>
2023-11-20 23:04:34 +0200

    'Solution'

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,42 @@
-from django.urls import path
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-from .views import index
+from taxi.models import Driver, Car, Manufacturer
 
-urlpatterns = [
-    path("", index, name="index"),
-]
 
-app_name = "taxi"
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+    }
+
+    return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
