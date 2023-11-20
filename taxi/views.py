7557defe84725785ed0ce29df7c3184dd5484186
Katherine-Greg <katiasimpson17@gmail.com>
2023-11-20 14:39:46 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -19,32 +19,27 @@ def index(request):
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
     context_object_name = "car_list"
-    template_name = "taxi/car_list.html"
     queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
 class CarDetailView(generic.DetailView):
     model = Car
-    template_name = "taxi/car_detail.html"
 
 
 class DriverListView(generic.ListView):
     model = Driver
     context_object_name = "driver_list"
-    template_name = "taxi/driver_list.html"
     paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars")
-    template_name = "taxi/driver_detail.html"
