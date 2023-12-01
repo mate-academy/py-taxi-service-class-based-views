ab86d2a56e82ed4e1956a8e11f5b890359562dec
Alona <alyonkabagritskaya@gmail.com>
2023-12-01 19:28:31 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,31 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    DriverListView,
+    CarDetailView,
+    DriverDetailView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/",
+         ManufacturerListView.as_view(),
+         name="manufacturers"),
+    path("cars/",
+         CarListView.as_view(),
+         name="cars"),
+    path("cars/<int:pk>/"
+         , CarDetailView.as_view(),
+         name="car_detail"),
+    path("drivers/",
+         DriverListView.as_view(),
+         name="drivers"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         fname="driver_detail"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
 
     context = {
@@ -13,3 +15,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    template_name = "taxi/Manufacturers_list.html"
+    context_object_name = "manufacturers_list"
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    template_name = "taxi/cars_list.html"
+    context_object_name = "cars_list"
+    paginate_by = 5
+
+
+class DriverListView(ListView):
+    model = Driver
+    template_name = "taxi/drivers_list.html"
+    context_object_name = "drivers_list"
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/cars_detail.html"
+    context_object_name = "cars_detail"
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "drivers_detail"
