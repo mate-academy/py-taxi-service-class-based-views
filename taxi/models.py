e3ebd5669e0283e567bf9527d4e92dd32d06bf06
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-14 00:52:07 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -6,9 +6,15 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"Name: {self.name} | Country: {self.country}"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.username
 
 
 class Car(models.Model):
