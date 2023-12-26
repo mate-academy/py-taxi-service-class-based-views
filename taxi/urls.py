8110b21486fbec0dd73a6403830c8eb0b620e87c
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-12-26 19:48:20 +0100

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from taxi.views import (ManufacturerListView,
-                        CarListView,
-                        CarDetailView,
-                        DriverListView,
-                        DriverDetailView)
+from taxi.views import (
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView
+)
 
 from .views import index
 
@@ -13,9 +15,11 @@ urlpatterns = [
          ManufacturerListView.as_view(),
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         name="driver-detail"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -18,13 +18,13 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.select_related("manufacturer").all()
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
