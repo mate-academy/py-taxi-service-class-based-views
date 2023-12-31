0d835773180ad1b23a43522362bf702eb924087d
Your_Name Your_Surname <your@email>
2023-12-31 16:02:05 +0200

    add last changes

--- taxi/urls.py
+++ taxi/urls.py
@@ -13,14 +13,14 @@ urlpatterns = [
     path("", index, name="index"),
     path("manufacturers/",
          ManufacturerListView.as_view(),
-         name="manufacturer_list"),
-    path("cars/", CarListView.as_view(), name="car_list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
-    path("drivers/", DriverListView.as_view(), name="driver_list"),
+         name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
-        name="driver_detail"),
+        name="driver-detail"),
 ]
 
 app_name = "taxi"
