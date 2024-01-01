5ae3256589abe30a0b74ee9fa88cdf18015c9ef1
Your_Name Your_Surname <your@email>
2024-01-01 11:07:21 +0200

    add bootstrap by requirements

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
          ManufacturerListView.as_view(),
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
-        name="driver-detail"),
+        name="driver_detail"),
 ]
 
 app_name = "taxi"
