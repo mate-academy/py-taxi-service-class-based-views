eb66b430e0b7afe5f95e3d68f91dab1dd2481193
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2024-01-11 09:03:05 -0600

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -37,5 +37,6 @@ class DriverListView(ListView):
 
 class DriverDetailView(DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related('cars')
-
+    queryset = (
+        Driver.objects.prefetch_related("cars")
+    )
