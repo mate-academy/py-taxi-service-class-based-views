cd9d04dfb44c68e5b93248571c423fd95816e582
Mikhail <tiunovmixs@gmail.com>
2024-01-16 15:40:05 +0200

    Solution Taxi service class-based views

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +16,38 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related("cars__manufacturer")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = self.object.cars.all()
+        return context
