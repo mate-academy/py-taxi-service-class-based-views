e49f8630d88c9b87648074077962724e066b902b
Vladyslav Shepilov <shepsterya@gmail.com>
2024-01-17 16:53:32 +0200

    done

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 
-from .views import index, ManufacturerListView, CarListView,\
-                   CarDetailView, DriverListView, DriverDetailView
-
+from .views import index, ManufacturerListView,\
+    CarListView, CarDetailView,\
+    DriverListView, DriverDetailView
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturers"),
-    path("cars/", CarListView.as_view(), name="cars"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver_detail"),  # Added the trailing slash
-    path("drivers/", DriverListView.as_view(), name="drivers")
+    path("manufacturers/", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path("cars/", CarListView.as_view(),
+         name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(),
+         name="car-detail"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
+    path("drivers/", DriverListView.as_view(),
+         name="driver-list")
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,35 +1,35 @@
+# views.py
+
 from django.shortcuts import render
 from django.views import generic
-
 from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
     """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
-
-
-class ManufacturerListView(generic.ListView):
-    model = Manufacturer
-    template_name = "manufacturer_list.html"
-    queryset = Manufacturer.objects.order_by("name")
-    paginate_by = 5
 
 
 class CarListView(generic.ListView):
     model = Car
-    template_name = "car_list.html"
+    template_name = "taxi/car_list.html"
     paginate_by = 5
 
     def get_queryset(self):
-        return Car.objects.prefetch_related("manufacturer")
+        return Car.objects.prefetch_related("manufacturer").order_by("id")
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/car_list.html"
+    paginate_by = 5
+    ordering = ["name"]
+    context_object_name = "manufacturer_list"
 
 
 class CarDetailView(generic.DetailView):
@@ -39,13 +39,13 @@ class CarDetailView(generic.DetailView):
 
 class DriverListView(generic.ListView):
     model = Driver
-    template_name = "driver_list.html"
+    template_name = "taxi/driver_list.html"
     paginate_by = 5
 
     def get_queryset(self):
-        return Driver.objects.all()
+        return Driver.objects.all().order_by("id")
 
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    template_name = "driver_detail.html"
+    template_name = "taxi/driver_detail.html"
