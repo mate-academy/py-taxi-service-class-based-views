43495db46d094f42183c10ff47b6016bce87582b
Maxim V <m4k51m15k4m@gmail.com>
2024-01-22 20:09:28 +0200

    all  done  and  works  except  html  content

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -6,10 +7,35 @@ from taxi.models import Driver, Car, Manufacturer
 def index(request):
     """View function for the home page of the site."""
 
-    context = {
-        "num_drivers": Driver.objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
-    }
+    return render(request,
+                  "taxi/index.html", {
+                      "num_drivers": Driver.objects.count(),
+                      "num_cars": Car.objects.count(),
+                      "num_manufacturers": Manufacturer.objects.count(), }
+                  )
 
-    return render(request, "taxi/index.html", context=context)
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by('name')
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related('manufacturer')
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related('cars__manufacturer')
