4bbbec110c4f5da299f512533bccfc06bbab9628
Максим <www.depaton@gmail.com>
2024-02-08 19:03:03 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView, CarListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer_list_view"),
+    path("cars/", CarListView.as_view(), name="cars_list_view"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,15 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
