147c8683cb85da1a44e6ff6468389266bbba3af9
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-13 00:16:59 +0200

    Delete django toolbar

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -37,13 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "taxi",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -136,5 +134,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "taxi.Driver"
-
-INTERNAL_IPS = ["127.0.0.1"]
