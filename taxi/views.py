7ca5844dbdbf82a76d5ad16bae6936d7a594c8a7
Kyrylo Storozhenko <busterakel@gmail.com>
2024-02-29 16:04:39 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -20,13 +20,13 @@ def index(request):
 
 class ManufacturerListView(ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
 class CarListView(ListView):
     model = Car
-    queryset = Car.objects.select_related("manufacturer").all()
+    queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
 
