04d48607dfc09f73b12fd0e58d7a4228575dc626
Artem Chernyshov <achernyshovv1@gmail.com>
2024-02-29 07:29:11 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
 
     context = {
         "num_drivers": Driver.objects.count(),
@@ -13,3 +12,47 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    template_name = "taxi/car_list.html"
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            "manufacturer": self.object.manufacturer,
+            "drivers": self.object.drivers.all(),
+        }
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+    template_name = "taxi/driver_list.html"
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    paginate_by = 5
+    template_name = "taxi/driver_detail.html"
+
+    def get_queryset(self):
+        return Driver.objects.prefetch_related("cars").all()
+
+    def get_context_data(self, **kwargs):
+        return {
+            "cars": self.object.cars.all(),
+        }
