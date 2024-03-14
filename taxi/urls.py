d2da9a6ccc2b30b67660aa64e0762bdc64627355
Molchanov Ihor <=>
2024-03-14 21:42:34 +0200

    solved task

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,17 +7,19 @@ from taxi.views import (index, ManufacturerListView,
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/",
-         ManufacturerListView.as_view(),
-         name="manufacturer-list"
-         ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(),
-         name="driver-list"),
-    path("drivers/<int:pk>/",
-         DriverDetailView.as_view(),
-         name="driver-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
 ]
 
 app_name = "taxi"
