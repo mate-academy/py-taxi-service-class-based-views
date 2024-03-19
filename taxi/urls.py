dfebe88712efa4d2ee22252b95b8f526152d8ed4
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-03-19 12:09:34 +0200

    implement-taxi-service-based-views

--- taxi/urls.py
+++ taxi/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(),
          name="car-list"),
-    path("cars/<int:pk>", CarDetailView.as_view(),
+    path("cars/<int:pk>/", CarDetailView.as_view(),
          name="car-detail"),
     path("drivers/", DriverListView.as_view(),
          name="driver-list"),
-    path("drivers/<int:pk>", DriverDetailView.as_view(),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
          name="driver-detail")
 ]
 
